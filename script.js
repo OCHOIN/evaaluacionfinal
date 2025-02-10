f1c9d38306038f3538af5bef9f074106ac98e6e0
OCHOIN <esteban.ochoa8693@utc.edu.ec>
2025-02-10 15:23:31 -0500

    intento uno

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (window.location.pathname.includes("latacunga.html")) {
+        alert("BIENVENIDO A LATACUNGA");
+    } else if (window.location.pathname.includes("mana.html")) {
+        alert("BIENVENIDOS A LA MANÁ");
+    } else if (window.location.pathname.includes("pangua.html")) {
+        alert("BIENVENIDO A PANGUA");
+    } else if (window.location.pathname.includes("pujili.html")) {
+        alert("BIENVENIDO A PUJILI");
+    } else if (window.location.pathname.includes("salcedo.html")) {
+        alert("BIENVENIDO A SALCEDO");
+    } else if (window.location.pathname.includes("sigchhos.html")) {
+        alert("BIENVENIDO A SIGCHOS");
+    } else if (window.location.pathname.includes("saquisili.html")) {
+        alert("BIENVENIDO A SAQUISILI");
+    }
+});
